8cc1224be70a47926a9e4b828c116f34395a5c33
MatteoGhisa777 <ghisalbertimatteo08@gmail.com>
2026-03-14 12:20:48 +0100

    esercizio2

---
+++ es2.java
@@ -0,0 +1,11 @@
+public class Variabili {
+    public static void main(String[] args) {
+        int numero = 10;
+        double prezzo = 19.99;
+        String messaggio = "Ciao, questo è un esempio";
+
+        System.out.println("Valore int: " + numero);
+        System.out.println("Valore double: " + prezzo);
+        System.out.println("Valore String: " + messaggio);
+    }
+}
